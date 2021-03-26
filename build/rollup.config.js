f7f7356787d6eebee42afb9e97b8a0ee21c9d71c
Maksym Nesterov <maxym.nes.dev@gmail.com>
2021-03-26 23:42:54 +0200

    Some attempt

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,22 +1,24 @@
 import commonjs from '@rollup/plugin-commonjs'
 import buble from '@rollup/plugin-buble'
 import vue from 'rollup-plugin-vue'
-import css from 'rollup-plugin-css-only'
+import scss from 'rollup-plugin-scss'
+// import css from 'rollup-plugin-css-only'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/wrapper.js',
   output: {
     name: 'VuePicker',
-    exports: 'named'
+    exports: 'named',
+    globals: { 'vue': 'Vue' },
   },
   plugins: [
-    css(),
-    commonjs(),
+    // css(),
     vue({
       css: false,
-      compileTemplate: true,
     }),
+    scss(),
+    commonjs(),
     buble(),
     (process.env.NODE_ENV === 'production' && terser()),
   ],
